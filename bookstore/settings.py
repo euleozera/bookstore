c148a9372f86ec76fdffaf641b4cdb7c9a14e1a6
Leonardo Melo <112220689+euleozera@users.noreply.github.com>
2022-11-21 18:54:37 -0300

    test authentication

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -135,5 +135,10 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,10 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from order.models import Order
-from order.serializers import OrderSeralizer
+from product.serializers.product_serializer import ProductSerializer
 
 
 class OrderViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProductSerializer
 
-    serializer_class = OrderSeralizer
-    queryset = Order.objects.all().order_by("id")
+    @property
+    def get_queryset(self):
+        return Order.objects.all().order_by("id")
